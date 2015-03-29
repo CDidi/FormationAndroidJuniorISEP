1a88eef4e90754b6f3ec73d396f427c7730fe706
floriantorel <florian.torel@gmail.com>
2015-03-29 18:45:48 +0200

    Test

--- app/src/main/java/android/junior/formation/fr/formationjuniorisep/MainActivity.java
+++ app/src/main/java/android/junior/formation/fr/formationjuniorisep/MainActivity.java
@@ -1,17 +1,24 @@
 package android.junior.formation.fr.formationjuniorisep;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
+    private ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myListView = (ListView) findViewById(R.id.myListView);
+
     }
 
 
